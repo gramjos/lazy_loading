c06a0dd3aee3a51865526983cd822c4cb93147ea
gramjos <graham.joss@gmail.com>
2025-05-20 08:56:35 -0500

    testing lazy load

---
+++ app.py
@@ -0,0 +1,10 @@
+transformers @ git+https://github.com/huggingface/transformers@46a4b7c909fab58355936e1c7109bb5e2e558267
+torch
+pdf2image
+annotated-types
+gradio
+huggingface-hub
+ipykernel
+ipython
+pillow
+pip
